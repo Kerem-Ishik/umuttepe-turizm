c40c80a07f41809393e78789929d9d2570978739
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-03-13 19:05:43 +0300

    Redesigned home page

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,17 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php /** @var array $userInfo */  ?>
-    <title><?= $userInfo['ad'] . ' ' . $userInfo['soyadi'] ?></title>
+    <title>Ana sayfa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         body {
+            display: flex;
+            min-height: 100svh;
+            flex-direction: column;
             font-family: Arial, sans-serif;
             background-color: #f8f9fa;
-        }
-
-        #navigation {
-            width: 38rem;
         }
 
         #e_posta {
@@ -21,44 +19,117 @@
         }
 
         #map {
-            height: 22rem;
+            height: 100%;
             width: 100%;
             margin-top: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
+
+        .navbar-brand {
+            user-select: none;
+            cursor: pointer;
+        }
     </style>
 </head>
 <body>
 <header>
-    <nav class="navbar w-100 px-2 shadow-sm">
+    <nav class="navbar navbar-expand-lg w-100 px-2 shadow-sm">
         <div class="container-fluid">
-            <a class="navbar-brand">Umuttepe Turizm</a>
+            <a class="navbar-brand" href="<?= base_url('/') ?>">Umuttepe Turizm</a>
 
-            <div class="d-flex gap-2 align-items-center">
-                <span id="e_posta"><?= $userInfo['e_posta'] ?></span>
-                <a role="button" href="<?= base_url('logout') ?>" class="btn btn-danger">Çıkış yap</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleContent" aria-controls="navbarToggleContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarToggleContent">
+                <div class="d-flex flex-row justify-content-center justify-content-lg-end w-100">
+                    <div class="d-flex flex-column flex-lg-row gap-2 align-items-center">
+                        <span id="e_posta" class="navbar-text"><?= session()->get('userInfo')['e_posta'] ?></span>
+                        <a role="button" href="<?= base_url('logout') ?>" class="btn btn-danger">Çıkış yap</a>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
 </header>
 
-<main class="p-4">
-    <div id="navigation" class="d-flex flex-column">
-        <label for="origin">Nereden</label>
-        <select id="origin" class="form-select mb-3">
-            <option selected disabled>Seçiniz</option>
-            <option value="İstanbul">İstanbul</option>
-            <option value="Ankara">Ankara</option>
-            <option value="İzmir">İzmir</option>
-            <option value="Antalya">Antalya</option>
-        </select>
-        <label for="destination">Nereye</label>
-        <select id="destination" class="form-select">
-            <option selected disabled>Seçiniz</option>
-        </select>
-        <div id="map"></div>
-        <h4 id="distance" class="mt-3 align-self-center"></h4>
+<main class="p-4 d-flex flex-fill align-items-center justify-content-center">
+    <div class="container flex-column flex-lg-row d-flex">
+        <div class="container">
+            <div id="map"></div>
+            <h4 id="distance" class="mt-3 align-self-center"></h4>
+        </div>
+        <div class="container">
+            <form action="<?= base_url('seferAra') ?>" method="post">
+                <div class="mb-3">
+                    <label for="origin">Nereden</label>
+                    <select id="origin" name="kalkis" class="form-select mb-3">
+                        <option selected disabled>Seçiniz</option>
+                        <option value="İstanbul">İstanbul</option>
+                        <option value="Ankara">Ankara</option>
+                        <option value="İzmir">İzmir</option>
+                        <option value="Antalya">Antalya</option>
+                    </select>
+                    <span class="text-danger">
+                        <?php if (! empty(session()->getFlashdata('validation'))) : ?>
+                            <?= display_error(session()->getFlashdata('validation'), 'kalkis') ?>
+                        <?php endif ?>
+                    </span>
+                </div>
+
+                <div class="mb-3">
+                    <label for="destination">Nereye</label>
+                    <select id="destination" name="varis" class="form-select">
+                        <option selected disabled>Seçiniz</option>
+                    </select>
+                    <span class="text-danger">
+                        <?php if (! empty(session()->getFlashdata('validation'))) : ?>
+                            <?= display_error(session()->getFlashdata('validation'), 'varis') ?>
+                        <?php endif ?>
+                    </span>
+                </div>
+
+                <div class="d-flex flex-row gap-3">
+                    <div class="form-check mb-3">
+                        <input class="form-check-input" type="radio" name="gidis_tipi" value="gidis" id="gidis" checked/>
+                        <label class="form-check-label" for="gidis">Gidiş</label>
+                    </div>
+
+                    <div class="form-check mb-3">
+                        <input class="form-check-input" type="radio" name="gidis_tipi" value="gidis_donus" id="gidis_donus" />
+                        <label class="form-check-label" for="gidis_donus">Gidiş-Dönüş</label>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-row gap-2 mb-3">
+                    <div class="flex-fill">
+                        <label for="gidis_tarihi">Gidiş Tarihi</label>
+                        <input type="date" name="gidis_tarihi" id="gidis_tarihi" class="form-control" min="<?= date('Y-m-d') ?>"/>
+                        <span class="text-danger">
+                            <?php if (! empty(session()->getFlashdata('validation'))) : ?>
+                                <?= display_error(session()->getFlashdata('validation'), 'gidis_tarihi') ?>
+                            <?php endif ?>
+                        </span>
+                    </div>
+
+                    <div class="flex-fill">
+                        <label for="donus_tarihi">Dönüş tarihi</label>
+                        <input type="date" name="donus_tarihi" id="donus_tarihi" class="form-control" min="<?= date('Y-m-d') ?>" disabled/>
+                    </div>
+                </div>
+
+                <div>
+                    <span class="text-danger">
+                            <?php if (! empty(session()->getFlashdata('validation'))) : ?>
+                                <?= display_error(session()->getFlashdata('validation'), 'donus_tarihi') ?>
+                            <?php endif ?>
+                        </span>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Sefer ara</button>
+            </form>
+        </div>
     </div>
 </main>
 
@@ -76,7 +147,7 @@
         const originInput = document.getElementById('origin');
         const destinationInput = document.getElementById('destination');
 
-        /** @param {InputEvent} event */
+        /** @param {Event} event */
         const onOriginChange = (event) => {
             if (event.target.value === "İstanbul") {
                 destinationInput.innerHTML =
@@ -127,7 +198,18 @@
             }
         });
     }
+
+    document.getElementsByName('gidis_tipi').forEach((radio) => {
+        radio.addEventListener('change', () => {
+            document.getElementById('donus_tarihi').disabled = radio.value === 'gidis';
+        });
+    });
+
+    window.onload = () => {
+        document.querySelector('form').reset();
+    }
 </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAlsV41_jlFTFhzWwTLy7TpeioSuggP7Q0&libraries=places&callback=initMap" async defer></script>
 </body>
 </html>
