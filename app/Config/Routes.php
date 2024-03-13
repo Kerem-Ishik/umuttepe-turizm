591784235231edd178cd82c2f6557b7bc6e6562b
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-03-13 19:03:44 +0300

    Add seferAra route to Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,11 +9,13 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->group('', ['filter' => 'AuthCheck'], function (RouteCollection $routes) {
     $routes->get('/', [Home::class, 'index']);
+    $routes->get('seferler', [Home::class, 'seferler']);
 });
 
 $routes->get('logout', [AuthController::class, 'logout']);
 $routes->post('save', [AuthController::class, 'save']);
 $routes->post('check', [AuthController::class, 'check']);
+$routes->post('seferAra', [Home::class, 'seferAra']);
 
 $routes->group('', ['filter' => 'AlreadyLoggedIn'], function (RouteCollection $routes) {
     $routes->get('login', [AuthController::class, 'login']);
