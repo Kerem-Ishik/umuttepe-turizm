8dd00f0421dd75504e2a3d235cd3f36deee4ab8a
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-03-21 21:56:39 +0300

    Add profile and update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,15 +16,19 @@ $routes->group('', ['filter' => 'AuthCheck'], function (RouteCollection $routes)
     $routes->get('koltuk-secimi', [KoltukController::class, 'index']);
     $routes->get('odeme', [OdemeController::class, 'index']);
     $routes->get('onay', [OdemeController::class, 'onay']);
+    $routes->get('profile', [AuthController::class, 'profile']);
+    $routes->get('profile/biletler', [AuthController::class, 'biletler']);
 });
 
 $routes->get('logout', [AuthController::class, 'logout']);
 $routes->post('save', [AuthController::class, 'save']);
 $routes->post('check', [AuthController::class, 'check']);
+$routes->post('update', [AuthController::class, 'update']);
 $routes->post('seferAra', [SeferController::class, 'seferAra']);
 $routes->post('koltukSecimi', [KoltukController::class, 'koltukSecimi']);
 $routes->post('odemeAl', [OdemeController::class, 'odemeAl']);
 $routes->post('odemeYap', [OdemeController::class, 'odemeYap']);
+$routes->delete('bilet-sil/(:num)', [AuthController::class, 'biletSil']);
 
 $routes->group('', ['filter' => 'AlreadyLoggedIn'], function (RouteCollection $routes) {
     $routes->get('login', [AuthController::class, 'login']);
