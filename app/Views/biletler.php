9c4be0337c1eb313ba9e541179d3f6cacb388adc
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-03-27 14:54:03 +0300

    Refactor biletler.php view layout and add QR code generation

--- app/Views/biletler.php
+++ app/Views/biletler.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        td {
+            line-height: 64px;
+            text-align: center;
+        }
+        th:not(:first-child) {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 
@@ -22,42 +31,49 @@
 
     <div class="container mt-5">
         <div class="row">
-            <div class="col-12">
-                <h2>Biletlerim</h2>
-                <hr>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Ad</th>
-                            <th scope="col">Soyadı</th>
-                            <th scope="col">Sefer</th>
-                            <th scope="col">Tarih</th>
-                            <th scope="col">Koltuk</th>
-                            <th scope="col">Alım Tarihi</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        /**
-                         * @var array $biletler
-                         */
-                        ?>
-                        <?php foreach ($biletler as $bilet): ?>
-                            <tr>
-                                <th scope="row"><?= $bilet['id'] ?></th>
-                                <td><?= $bilet['ad'] ?></td>
-                                <td><?= $bilet['soyadi'] ?></td>
-                                <td><?= $bilet['sefer'] ?></td>
-                                <td><?= $bilet['tarih'] ?></td>
-                                <td><?= $bilet['koltuk_no'] ?></td>
-                                <td><?= $bilet['alim_tarihi'] ?></td>
-                                <td><button type="button" class="btn-close" aria-label="Sil" data-bs-toggle="modal" data-bs-target="#deleteConfirm" onclick="clickedId = <?= $bilet['id'] ?>"></button></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+            <h2>Biletlerim</h2>
+            <hr>
+            <div class="row d-flex flex-column flex-lg-row">
+                <?php
+                /**
+                 * @var array $biletler
+                 */
+                ?>
+
+                <?php foreach ($biletler as $bilet): ?>
+                    <div class="container col-lg-6 mb-4">
+                        <div class="d-flex flex-row align-items-center gap-4 p-3 card shadow-sm">
+                            <div class="qrcode"></div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="fw-light fs-6">Ad-Soyadı</div>
+                                    <?php
+                                    $soyadi = strlen($bilet['soyadi']) > 7 ? mb_substr($bilet['soyadi'], 0, 7).'...' : $bilet['soyadi'];
+                                    ?>
+                                    <div class="fw-semibold fs-5" title="<?= "{$bilet['ad']} {$bilet['soyadi']}" ?>"><?= "{$bilet['ad']} $soyadi" ?></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="fw-light fs-6">Sefer</div>
+                                    <div class="fw-semibold fs-5"><?= $bilet['sefer'] ?></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="fw-light fs-6">Sefer Tarihi</div>
+                                    <div class="fw-semibold fs-5"><?= $bilet['tarih'] ?></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="fw-light fs-6">Koltuk Numarası</div>
+                                    <div class="fw-semibold fs-5"><?= $bilet['koltuk_no'] ?></div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="fw-light fs-6">Alım Tarihi</div>
+                                    <div class="fw-semibold fs-5"><?= $bilet['alim_tarihi'] ?></div>
+                                </div>
+                                <p hidden="hidden" class="pnr"><?= $bilet['pnr'] ?></p>
+                            </div>
+                            <button type="button" class="btn-close" aria-label="Sil" data-bs-toggle="modal" data-bs-target="#deleteConfirm" onclick="clickedId = <?= $bilet['id'] ?>"></button>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
@@ -81,6 +97,8 @@
     </div>
 </main>
 
+<script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
+
 <script>
     let clickedId = 0;
 
@@ -91,6 +109,14 @@
             location.reload();
         });
     }
+
+    document.querySelectorAll('.qrcode').forEach(element => {
+        new QRCode(element, {
+            text: "<?= base_url('bilet-kontrol').'/' ?>" + element.parentElement.parentElement.querySelector('.pnr').innerText,
+            width: 128,
+            height: 128
+        });
+    });
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
